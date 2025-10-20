a359cd79bd3b5e4f2ba8986d106dde8d5df17bf6
Gia Nhat <112574262+GiaNhat688@users.noreply.github.com>
2025-10-20 12:20:00 +0700

    Update main.c

--- BT12_Queue/main.c
+++ BT12_Queue/main.c
@@ -20,30 +20,30 @@ void GPIO_Config(void) {
     GPIOA->CRL |= (0x1 << 20);
 }
 
-void vTask_LED(void *pvParameters) {
+void vTask_LED(void *pvParameters){
     PWM_Params_t params;
-    for (;;) {
-        if (xQueueReceive(xQueue, &params, portMAX_DELAY) == pdTRUE) {
+    for (;;){
+        if (xQueueReceive(xQueue, &params, portMAX_DELAY) == pdTRUE){
             float period_ms = 1000.0f / params.freq;
             float on_time = period_ms * params.duty / 100.0f;
             float off_time = period_ms - on_time;
             uint32_t cycles = params.freq;
             for (uint32_t i = 0; i < cycles; i++) {
-                GPIOA->BSRR = (1 << 5);
+                GPIOA->ODR |= 1<<5;
                 vTaskDelay(pdMS_TO_TICKS(on_time));
-                GPIOA->BRR = (1 << 5);
+                GPIOA->ODR &= ~(1<<5);
                 vTaskDelay(pdMS_TO_TICKS(off_time));
             }
         }
     }
 }
 
-void vTask_Generator(void *pvParameters) {
+void vTask_Generator(void *pvParameters){
     PWM_Params_t params;
     uint16_t freq_list[] = {1, 2, 5, 8, 10};
     uint8_t duty_list[] = {10, 30, 50, 70, 90};
     uint8_t idx = 0;
-    for (;;) {
+    for (;;){
         params.freq = freq_list[idx % 5];
         params.duty = duty_list[idx % 5];
         idx++;
@@ -52,7 +52,7 @@ void vTask_Generator(void *pvParameters) {
     }
 }
 
-int main(void) {
+int main(){
     GPIO_Config();
     xQueue = xQueueCreate(5, sizeof(PWM_Params_t));
     if (xQueue == NULL)
@@ -60,5 +60,4 @@ int main(void) {
     xTaskCreate(vTask_LED, "LED", 128, NULL, 1, NULL);
     xTaskCreate(vTask_Generator, "Gen", 128, NULL, 1, NULL);
     vTaskStartScheduler();
-    while (1);
 }
