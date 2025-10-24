f77af05cd604f219bfcc319bdfd9614cc536aff1
Gia Nhat <112574262+GiaNhat688@users.noreply.github.com>
2025-10-24 12:40:24 +0700

    Update main.c

--- B13_FreeRTOS_Mutex_IRQ/Bài 1/main.c
+++ B13_FreeRTOS_Mutex_IRQ/Bài 1/main.c
@@ -5,45 +5,44 @@
 #include <stdbool.h>
 
 SemaphoreHandle_t xInterruptSemaphore;
-volatile bool isWarningOn = false;
+volatile bool isOn = false;
 #define DEBOUNCE_TIME_MS 200
 
-void Hardware_Init(void){
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN| RCC_APB2ENR_AFIOEN;
-
-    GPIOA->CRL &= ~(0xF << 20);
-    GPIOA->CRL |= (0x3 << 20);
-
-    GPIOA->CRL &= ~(0xF << 4);
-    GPIOA->CRL |= (0x3 << 4);
+void Hardware_Init(void) {
+    RCC->APB2ENR |= (1 << 0) | (1 << 2);
 
     GPIOA->CRL &= ~(0xF << 0);
     GPIOA->CRL |= (0x8 << 0);
     GPIOA->ODR |= (1 << 0);
 
-    AFIO->EXTICR[0] &= ~AFIO_EXTICR1_EXTI0;
-    EXTI->IMR |= EXTI_IMR_MR0;
-    EXTI->FTSR |= EXTI_FTSR_TR0;
+    GPIOA->CRL &= ~(0xF << 4);
+    GPIOA->CRL |= (0x3 << 4);
+
+    GPIOA->CRL &= ~(0xF << 20);
+    GPIOA->CRL |= (0x3 << 20);
+
+    AFIO->EXTICR[0] &= ~(0xF << 0);
+    EXTI->IMR |= (1 << 0);
+    EXTI->FTSR |= (1 << 0);
 
     NVIC_SetPriority(EXTI0_IRQn, 5);
     NVIC_EnableIRQ(EXTI0_IRQn);
 }
 
-void vBlinkTask(void *pvParameters){
-    for(;;){
+void vBlinkTask(void *pvParameters) {
+    for (;;) {
         GPIOA->ODR ^= (1 << 5);
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
-void vWarningTask(void *pvParameters){
-    for (;;){
-        if (xSemaphoreTake(xInterruptSemaphore, portMAX_DELAY) == pdTRUE){
-            isWarningOn = !isWarningOn;
-
-            if (isWarningOn){
-                GPIOA->BSRR = (1 << 1);
-            } else{
+void vInterruptTask(void *pvParameters) {
+    for (;;) {
+        if (xSemaphoreTake(xInterruptSemaphore, portMAX_DELAY) == pdTRUE) {
+            isOn = !isOn;
+            if (isOn) {
+                GPIOA->BSRR |= (1 << 1);
+            } else {
                 GPIOA->BRR = (1 << 1);
             }
         }
@@ -51,31 +50,28 @@ void vWarningTask(void *pvParameters){
 }
 
 void EXTI0_IRQHandler(void) {
-    static TickType_t lastInterruptTime = 0;
-    TickType_t currentInterruptTime = xTaskGetTickCountFromISR();
+    static TickType_t lastTime = 0;
+    TickType_t currentTime = xTaskGetTickCountFromISR();
 
-    if(EXTI->PR & EXTI_PR_PR0){
-        EXTI->PR |= EXTI_PR_PR0;
+    if (EXTI->PR & (1 << 0)) {
+        EXTI->PR |= (1 << 0);
     }
 
-    if((currentInterruptTime - lastInterruptTime) > pdMS_TO_TICKS(DEBOUNCE_TIME_MS)){
-        lastInterruptTime = currentInterruptTime;
-
+    if ((currentTime - lastTime) > pdMS_TO_TICKS(DEBOUNCE_TIME_MS)) {
+        lastTime = currentTime;
         BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-			
         xSemaphoreGiveFromISR(xInterruptSemaphore, &xHigherPriorityTaskWoken);
-			
         portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
     }
 }
 
-int main(void){
+int main(void) {
     Hardware_Init();
     xInterruptSemaphore = xSemaphoreCreateBinary();
 
-    if(xInterruptSemaphore != NULL){
+    if (xInterruptSemaphore != NULL) {
         xTaskCreate(vBlinkTask, "Blink", 128, NULL, 1, NULL);
-        xTaskCreate(vWarningTask, "Warning", 128, NULL, 2, NULL);
+        xTaskCreate(vInterruptTask, "Interrupt", 128, NULL, 2, NULL);
         vTaskStartScheduler();
     }
 
