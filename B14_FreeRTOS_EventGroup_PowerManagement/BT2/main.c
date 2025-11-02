5f5b286a410ea70361982976ce7525f7d9c54f8a
Gia Nhat <112574262+GiaNhat688@users.noreply.github.com>
2025-11-03 03:12:17 +0700

    Update main.c

--- B14_FreeRTOS_EventGroup_PowerManagement/BT2/main.c
+++ B14_FreeRTOS_EventGroup_PowerManagement/BT2/main.c
@@ -1,1 +1,45 @@
+#include "stm32f10x.h"
 
+volatile uint32_t g_delay_counter = 0;
+
+void GPIO_Init(void){
+    RCC->APB2ENR |= (1<<2);
+	
+    GPIOA->CRL &= ~(0xF<<0);;
+    GPIOA->CRL |= (0x3<<0); 
+}
+
+void SysTick_Init(void){
+    SysTick->LOAD = 8000 - 1;
+		SysTick->VAL = 0;
+		SysTick->CTRL |= (1<<0|1<<1|1<<2);
+}
+
+void SysTick_Handler(void)
+{
+    if(g_delay_counter > 0){
+			g_delay_counter--;
+		}	
+}
+
+void Delay_ms_sleep(uint32_t ms){
+    g_delay_counter = ms;
+    while(g_delay_counter > 0){
+        __WFI(); 
+    }
+}
+
+int main(void){
+    GPIO_Init();
+    SysTick_Init();
+	
+    SCB->SCR &= ~(1<<2);
+
+    while(1){
+        GPIOA->BSRR |= (1<<0); 
+        Delay_ms_sleep(1000);
+			
+        GPIOA->BRR |= (1<<0); 
+        Delay_ms_sleep(1000);
+    }
+}
